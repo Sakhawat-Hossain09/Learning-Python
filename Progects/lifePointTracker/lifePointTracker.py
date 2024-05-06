4155d25c4541d80737e89bda5559b5b386facb18
Sakhawat  Hossain <sakhawathossain3141@gmail.com>
2024-05-06 12:19:27 +0000

    lifePointTracker update

--- Progects/lifePointTracker/lifePointTracker.py
+++ Progects/lifePointTracker/lifePointTracker.py
@@ -9,19 +9,28 @@ def convertToint(filename):
   except ValueError:
     print(f"Error: The content of '{filename}' is not a valid number.")
     return None
-convertToint()
+
+def commendInput():
+  print("Available functions are listed below:")
+  print("1. check balance ['get balance -a']")
+  print("2. See what can be bought ['check avltm -a']")
+  print("2. Add to your balance ['add balance ('Amount')']")
+  userRequest = input("What do you want to do: ")
+  if userRequest != " ":
+    return None
+
+  else:
+    print("Redo the procces.")
+    commendInput()
+    return str(userRequest)
+
+commendInput()
 shortFormVideo  = ("You can now watch 10 short form videos.", 10)
 longVideo = ("You can now watch 1 long form video. ", 10)
-print("Available functions are listed below:")
-print("1. check balance ['get balance -a']")
-print("2. See what can be bought ['check avltm -a']")
-print("2. Add to your balance ['add balance ('Amount')']")
-userRequest = input("What do you want to do: ")
+
+
 pointsFile = "Progects/lifePointTracker/lifePointTracker.txt"
 pointsInfo = open(pointsFile, 'r')
 points = int(pointsInfo)
-if userRequest == "get balence -a":
-    print(f"Your point is {points.read()}")
-elif userRequest == "check avltm -a":
-  if points >= shortFormVideo[1]:
-     print(shortFormVideo[0])
+
+convertToint("Progects//lifePointTracker//lifePointTracker.txt")
