5bf5d21e11923a808bf4f69713239cdf8029ca85
Sakhawat  Hossain <sakhawathossain3141@gmail.com>
2024-09-06 06:40:08 -0700

    Update Learning.py

---
+++ Learning.py
@@ -0,0 +1,8 @@
+"""
+My Homework for video 1:
+1. Edit the script so that users can also enter their expenses (eg. rent, food, gym memberships) so they
+can see how much they have left over each month.
+2. Recreate the user input section to safely handle users inserting the wrong type without 
+crashing the program.
+
+"""
