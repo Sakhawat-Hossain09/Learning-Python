88a512845e5eee598e5a8c2e2fb4dfc1104333d2
Sakhawat Hossain <spacelover9696@gmail.com>
2024-09-26 16:25:53 +0600

    finished finance_calculator

--- Progects/finance_calculator.py
+++ Progects/finance_calculator.py
@@ -1,19 +1,28 @@
-"""
-My Homework for video 1:
-1. Edit the script so that users can also enter their expenses (eg. rent, food, gym memberships) so they
-can see how much they have left over each month.
-2. Recreate the user input section to safely handle users inserting the wrong type without 
-crashing the program.
-
-"""
+#My Homework for video 1:
+#1. Edit the script so that users can also enter their expenses (eg. rent, food, gym memberships) so they
+#can see how much they have left over each month.
+   
 def finance_calculator(monthly_earning: float, tax_rate: float, currency: str) -> None:
+    """
+    Calculates and prints the monthly and yearly financial information based on the given
+    monthly earning, tax rate, and currency.
+
+    Args:
+        monthly_earning: The monthly income of the user.
+        tax_rate: The tax rate applied to the monthly income.
+        currency: The currency used for the calculations.
+    """
+
+    # Calculate monthly deductions and net earnings
     monthly_tax_deduction = monthly_earning * (tax_rate / 100)
     monthly_net_earning = monthly_earning - monthly_tax_deduction
-    yearly_earning: float = monthly_earning * 12
+
+    # Calculate yearly values
+    yearly_earning = monthly_earning * 12
     yearly_tax_deduction = monthly_tax_deduction * 12
     yearly_net_earning = monthly_net_earning * 12
 
-
+    # Print the results
     print("----------------------------------------------------------------")
     print(f"Your monthly earning is: {monthly_earning}.")
     print(f"Your tax rate is {tax_rate}.")
@@ -25,6 +34,27 @@ def finance_calculator(monthly_earning: float, tax_rate: float, currency: str) -
     print("----------------------------------------------------------------")
 
 
+def main():
+    """
+    The main function that handles user input and calls the finance calculator.
+    """
+
+    while True:
+        try:
+            # Get user input for currency, monthly earning, and tax rate
+            currency = str(input("Enter your currency: ").upper())
+            monthly_earning = float(input(f"Enter your monthly earning (in {currency}) (Use \"_\" as thousand separator ex: 10_000): "))
+            tax_rate = float(input("Enter your tax rate (in %): "))
+
+            # Break the loop if input is valid
+            break
+        except ValueError:
+            # Handle invalid input and continue the loop
+            print("Invalid input. Please enter a number for monthly earning and tax rate.")
+
+    # Call the finance calculator with the user input
+    finance_calculator(monthly_earning=monthly_earning, tax_rate=tax_rate, currency=currency)
 
 
-#finance_calculator(monthly_earning=10000, tax_rate=35.7, currency="USD")
+if "__main__" == __name__:
+    main()
