8642c07cdd3393a1036b11dbfefa2de7b9cdc0d6
Sakhawat Hossain <spacelover9696@gmail.com>
2024-09-28 17:26:10 +0600

    clean

--- Progects/convertTemperature.py
+++ Progects/convertTemperature.py
@@ -1,36 +1,43 @@
 def convertTemperature():
-    enteredTemperature = int(input("What is the temperature you want to convert: "))
-    enteredTemperatureUnit = input("Which unit is the temperature in [Celsius(C), Fahrenheit(F), Kelvin(K)]: ").upper()
-    conversionTemperatureUnit = input("In which unit do you want the temperature to be in [Celsius(C), Fahrenheit(F), Kelvin(K)]: ").upper()
-    if enteredTemperatureUnit == "C":
-        if enteredTemperature < -273.15:
+    entered_temperature = int(input("What is the temperature you want to convert: "))
+    entered_temperature_unit = input("Which unit is the temperature in [Celsius(C), Fahrenheit(F), Kelvin(K)]: ").upper()
+    conversion_temperature_unit = input("In which unit do you want the temperature to be in [Celsius(C), Fahrenheit(F), Kelvin(K)]: ").upper()
+    if entered_temperature_unit == "C":
+        if entered_temperature < -273.15:
             print("Temperature below -273.15 doesn't exist in Celsius scale.")
         else:
-            if conversionTemperatureUnit == "K":
-                convertedTemperature = enteredTemperature + 273.15
-                print(f"The temperature in Kelvin is: {convertedTemperature:,.3f}")
-            elif conversionTemperatureUnit == "F":
-                convertedTemperature = (9 * enteredTemperature) / 5 + 32
-                print(f"The temperature in Fahrenheit is: {convertedTemperature:,.3f}")
-    elif enteredTemperatureUnit == "F":
-        if enteredTemperature < -459.67:
+            if conversion_temperature_unit == "K":
+                entered_temperature_unit = entered_temperature + 273.15
+                print(f"The temperature in Kelvin is: {entered_temperature_unit:,.3f}")
+            elif conversion_temperature_unit == "F":
+                entered_temperature_unit = (9 * entered_temperature) / 5 + 32
+                print(f"The temperature in Fahrenheit is: {entered_temperature_unit:,.3f}")
+    elif entered_temperature_unit == "F":
+        if entered_temperature < -459.67:
             print("Temperature below -459.67 doesn't exist in Fahrenheit scale.")
         else:
-            if conversionTemperatureUnit == "C":
-                convertedTemperature = (enteredTemperature - 32) * 5 / 9
-                print(f"The temperature in Celsius is: {convertedTemperature:,.3f}")
-            elif conversionTemperatureUnit == "K":
-                convertedTemperature = ((enteredTemperature - 32) * 5 / 9) + 273.15
-                print(f"The temperature in Kelvin is: {convertedTemperature:,.3f}")
-    elif enteredTemperatureUnit == "K":
-        if enteredTemperature < 0:
+            if conversion_temperature_unit == "C":
+                entered_temperature_unit = (entered_temperature - 32) * 5 / 9
+                print(f"The temperature in Celsius is: {entered_temperature_unit:,.3f}")
+            elif conversion_temperature_unit == "K":
+                entered_temperature_unit = ((entered_temperature - 32) * 5 / 9) + 273.15
+                print(f"The temperature in Kelvin is: {entered_temperature_unit:,.3f}")
+    elif entered_temperature_unit == "K":
+        if entered_temperature < 0:
             print("Temperature below 0 doesn't exist in Kelvin scale.")
         else:
-            if conversionTemperatureUnit == "C":
-                convertedTemperature = enteredTemperature - 273.15
-                print(f"The temperature in Celsius is: {convertedTemperature:,.3f}")
-            elif conversionTemperatureUnit == "F":
-                convertedTemperature = (9 * (enteredTemperature - 273.15)) / 5 + 32
-                print(f"The temperature in Fahrenheit is: {convertedTemperature:,.3f}")
+            if conversion_temperature_unit == "C":
+                entered_temperature_unit = entered_temperature - 273.15
+                print(f"The temperature in Celsius is: {entered_temperature_unit:,.3f}")
+            elif conversion_temperature_unit == "F":
+                entered_temperature_unit = (9 * (entered_temperature - 273.15)) / 5 + 32
+                print(f"The temperature in Fahrenheit is: {entered_temperature_unit:,.3f}")
     else:
         print("Encountered unknown unit. Please try again.")
+
+
+def main():
+    convertTemperature()
+
+if __name__ == "__main__":
+    main()
