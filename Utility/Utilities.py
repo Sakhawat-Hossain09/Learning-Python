898f4a2907838c5cc9ff97cd8623500a7bda6d6e
Abdul Alim <abdul.alim@datacrafters.io>
2024-10-14 13:01:19 +0600

    i forgot what i did

--- Utility/Utilities.py
+++ Utility/Utilities.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 class Progects:
     def __init__(self):
@@ -68,6 +69,23 @@ class Progects:
         return converted_temperature
 
 
+    def countdownProgram(self, starttingTime: str) -> None:
+
+        try:
+            enteredHour = int(starttingTime[0:2])
+            enteredMinute = int(starttingTime[3:5])
+            enteredSecond = int(starttingTime[6:8])       
+            totalSeconds = enteredHour*3600+enteredMinute*60+enteredSecond
+            for i in range(totalSeconds, 0, -1):
+                displayingSecond = i%60
+                displayingMinute = int(i/60)%60
+                displayingHour = int(i/3600)%60
+                print(f"{displayingHour:02}:{displayingMinute:02}:{displayingSecond:02}")
+                time.sleep(1)
+            print("Countdown finished successfully.")
+        except: ValueError
+        print("Check if entered values are correct.")
+
 
 
 
@@ -93,6 +111,7 @@ class Mathematical:
         """
         arc_distance: float = (theta_degrees / 360) * 2 * math.pi * radius
         return arc_distance
+
 
     def calculate_circle_area(self, radius: float) -> float:
         """
@@ -266,9 +285,9 @@ def main():
 
     progects = Progects()
 
-    progects.concession_stand_program()
+    # progects.concession_stand_program()
     print(progects.convertTemperature(entered_temperature=10, entered_temperature_unit="c", conversion_temperature_unit="f"))
-
+    progects.countdownProgram(starttingTime="60:41:31")
 
 
 
